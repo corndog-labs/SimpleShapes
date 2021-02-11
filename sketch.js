219786a75e355f9acdc889d4c085bd616d111351
corndog-labs <56854559+corndog-labs@users.noreply.github.com>
2021-02-11 14:55:29 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,60 +2,36 @@
 //-- Sample P5.js Sketch for drawing things!
 
 
-// Global Variables
-var ellipseXPos = 0;
-var ellipseYPos = 300;
-var ellipseDist = 40;
-
-// Setup code goes here
 function setup() {
-  createCanvas(1000, 800);
-  print("Starting up Simple Shapes");
-  rectMode(CENTER);
-  ellipseMode(CENTER);
+  createCanvas(700, 700);
+  strokeWeight(1);
 
-  print("Width = " + width);
-  print("Height = " + height);
+}
 
-  // set ellipse X pos
-  ellipseXPos = width/2 - 250;
- }
-
-// Draw code goes here
 function draw() {
-  let i = 5;
-  // do stuff with i
 
-  background(34,50,204);
+  background(255, 110, 110);
 
-  // Circle
-  fill(0,102,255);
-  circle(width/2,height/2,20);
+
+  for (var i = 0; i < width; i = i + 50) { 
+   fill (80, 120, 200 );
+   ellipse (i, 80, 80, 120);
+  }
+
+
+ 
+  fill(200, 40, 40);
+  ellipse(width/2, height/2, width/2, height/2);
+
+  fill(20, 200, 40);
+  ellipse(300, 300, width/4, height/4 );
+
+
+for (var i = 0; i < width; i = i + 100) {
+   fill(200, 90, 20);
+   rect(i, 500, 20, 150, 80);
+   }
+fill (30,60,80);
+   quad(38, 31, 86, 20, 69, 63, 30, 76);
   
-  drawEllipses();
-  drawRects();
 }
-
-function drawEllipses() {
-  // Ellipse #1
-  fill(0,255,0);
-  ellipse(ellipseXPos,ellipseYPos,200,80);
-
-  // Ellipse
-  fill(0,255,0);
-  ellipse(ellipseXPos,ellipseYPos+ellipseDist,200,80);
-}
-
-function drawRects() {
- // Center Rectangle
-  fill(255,0,0);
-  stroke(220);
-  strokeWeight(3);
-  rect( width/2,height/2,200,100);
-
-  // Tall Rect
-  fill(126,50,20);
-  stroke(27);
-  strokeWeight(2);
-  rect( 100,height/2,40,400);
- }
